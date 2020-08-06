d874c46fbee14f770d5a2bbcf787cacb17eb3dac
MarcM-collab <58913562+MarcM-collab@users.noreply.github.com>
2020-08-06 13:11:12 +0200

    Audio + screenShake

---
+++ RewindParty/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)] public float volume;
+    [Range(0f, 3f)] public float pitch;
+
+    [HideInInspector]public AudioSource source;
+}
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager AudioInstance;
+    void Awake()
+    {
+        if (AudioInstance == null)
+        {
+            AudioInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
---
+++ RewindParty/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float force;
+    public float duration;
+    private float cooldown;
+    private void Start()
+    {
+        cooldown = duration;
+    }
+    //Per fer la funció: Camera.main.GetComponent<CameraShake>().Shake();
+    public void Shake()
+    {
+        StartCoroutine(StartShaking());
+    }
+    private IEnumerator StartShaking()
+    {
+        Vector3 initialPos = transform.localPosition;
+        cooldown = duration;
+
+        while (cooldown > 0)
+        {
+            float xShakePos = initialPos.x + Random.Range(-1f, 1f) * force;
+            float yShakePos = initialPos.y + Random.Range(-1f, 1f) * force;
+
+            transform.localPosition = new Vector3(xShakePos, yShakePos, initialPos.z);
+            cooldown -= Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = initialPos; //avoid bugs, camera can end placed in local position wich is not the one we want so we assign the initial one.
+    }
+}
