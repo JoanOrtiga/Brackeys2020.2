3fce19978eafdf285920b9b14af8113ddd381836
MarcM-collab <58913562+MarcM-collab@users.noreply.github.com>
2020-08-07 19:44:25 +0200

    bloodfx

---
+++ RewindParty/Assets/DestroySelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    [SerializeField] private float destroyTime = 1;
+
+    private void Awake()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+}
