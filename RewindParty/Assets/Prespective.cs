cae1ff1fc702a3b25dc0d04e458c71798cd0079b
MarcM-collab <58913562+MarcM-collab@users.noreply.github.com>
2020-08-08 02:41:11 +0200

    merge

---
+++ RewindParty/Assets/Prespective.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prespective : MonoBehaviour
+{
+    [SerializeField] private int sortingBaseOrder = 5000;
+    [SerializeField] private int offset = 0;
+
+    private Renderer myRenderer;
+
+    private void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+    private void LateUpdate()
+    {
+        myRenderer.sortingOrder = (int)(sortingBaseOrder - transform.position.y - offset);
+    }
+}
